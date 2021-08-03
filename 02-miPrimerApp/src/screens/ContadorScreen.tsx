56cdcc07bd85f6f6ae196e8560b0f4182f81f30b
jonathan <yoni_12rrs@hotmail.com>
2021-08-03 17:40:35 -0500

    StyleSheet

--- 02-miPrimerApp/src/screens/ContadorScreen.tsx
+++ 02-miPrimerApp/src/screens/ContadorScreen.tsx
@@ -1,30 +1,37 @@
 import React, { useState } from 'react'
-import {  Text, TouchableOpacity, View } from 'react-native'
+import { Text, TouchableOpacity, View, StyleSheet } from 'react-native'
 
 export const ContadorScreen = () => {
 
     const [contador, setContador] = useState(10);
 
     return (
-        <View style={{
-            flex: 1,
-            justifyContent: 'center'
-        }}>
-            <Text style={{
-                fontSize: 40,
-                textAlign: 'center',
-                top: -10
-            }}>Contador: {contador}</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                Contador: {contador}</Text>
             <TouchableOpacity
-                onPress={() => setContador(contador+1)}
+                onPress={() => setContador(contador + 1)}
             >
-                <View style={{
-                    backgroundColor: 'red',
-                    borderRadius: 100
-                }}>
+                <View style={styles.buttonIncrementar}>
                     <Text>+1</Text>
                 </View>
             </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 40,
+        textAlign: 'center',
+        top: -10
+    },
+    buttonIncrementar: {
+        backgroundColor: 'red',
+        borderRadius: 100
+    }
+})
