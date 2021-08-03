07721b9355cbf1fe31f900b4bf2baa4d23432166
jonathan <yoni_12rrs@hotmail.com>
2021-08-03 18:52:15 -0500

    Botón personalizado flotante

--- 02-miPrimerApp/src/screens/ContadorScreen.tsx
+++ 02-miPrimerApp/src/screens/ContadorScreen.tsx
@@ -10,10 +10,19 @@ export const ContadorScreen = () => {
             <Text style={styles.title}>
                 Contador: {contador}</Text>
             <TouchableOpacity
+            style={styles.fabLocationBR}
                 onPress={() => setContador(contador + 1)}
             >
-                <View style={styles.buttonIncrementar}>
-                    <Text>+1</Text>
+                <View style={styles.fab}>
+                    <Text style={styles.fabText}>+1</Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity
+            style={styles.fabLocationBL}
+                onPress={() => setContador(contador - 1)}
+            >
+                <View style={styles.fab}>
+                    <Text style={styles.fabText}>-1</Text>
                 </View>
             </TouchableOpacity>
         </View>
@@ -23,15 +32,36 @@ export const ContadorScreen = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        backgroundColor: 'white'
     },
     title: {
         fontSize: 40,
+        top: -10,
         textAlign: 'center',
-        top: -10
     },
-    buttonIncrementar: {
-        backgroundColor: 'red',
-        borderRadius: 100
+    fabLocationBR: {
+        position: 'absolute',
+        bottom: 25,
+        right: 25
+    },
+    fabLocationBL: {
+        position: 'absolute',
+        bottom: 25,
+        left: 25
+    },
+    fab: {
+        backgroundColor: '#5856D6',
+        width: 60,
+        height: 60,
+        borderRadius: 100,
+        justifyContent: 'center'
+    },
+    fabText: {
+        color: 'white',
+        fontSize: 25,
+        fontWeight: 'bold',
+        alignSelf: 'center',
     }
+    
 })
