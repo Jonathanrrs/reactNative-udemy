0d38d3659929b4b30c85e7356f7e6a3e63d533f0
jonathan <yoni_12rrs@hotmail.com>
2021-08-10 21:18:26 -0500

    Enviar argumentos entre pantallas

--- 04-navegacionApp/src/screens/Pagina1Screen.tsx
+++ 04-navegacionApp/src/screens/Pagina1Screen.tsx
@@ -1,6 +1,6 @@
 import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, TouchableOpacity, View } from 'react-native';
 import { styles } from '../theme/appTheme';
 
 interface Props extends StackScreenProps<any, any>{};
@@ -13,7 +13,17 @@ export const Pagina1Screen = ({navigation}: Props) => {
             <Button 
                 title="Ir a página 2"
                 onPress={() => navigation.navigate('Pagina2Screen')}
-            />
+                />
+            <Text>Navegar con argumentos</Text>
+            <TouchableOpacity 
+                onPress={() => navigation.navigate('PersonaScreen', {
+                    id: 1,
+                    name: 'Jonathan'
+                })}
+
+            >
+                <Text>Jonathan</Text>
+            </TouchableOpacity>
         </View>
     )
 }
