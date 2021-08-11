4c707b5bd1783244c1cece04e9580ba32f35049a
jonathan <yoni_12rrs@hotmail.com>
2021-08-10 19:19:57 -0500

    React Navigation - Stack

---
+++ 04-navegacionApp/src/screens/Pagina3Screen.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const Pagina3Screen = () => {
+    return (
+        <View>
+            <Text>Página3Screen</Text>
+        </View>
+    )
+}
