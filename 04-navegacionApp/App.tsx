836ec5c094dc920af76e1fe6a2a07760e53a1793
jonathan <yoni_12rrs@hotmail.com>
2021-08-16 18:45:48 -0500

    Crear nuestro primer Context en TypeScript

--- 04-navegacionApp/App.tsx
+++ 04-navegacionApp/App.tsx
@@ -4,13 +4,25 @@ import { NavigationContainer } from '@react-navigation/native';
 // import { StackNavigator } from './src/navigator/StackNavigator';
 // import { MenuLateralBasico } from './src/navigator/MenuLateralBasico';
 import { MenuLateral } from './src/navigator/MenuLateral';
+import { AuthProvider } from './src/context/AuthContext';
 
  const App = () => {
   return (
     <NavigationContainer>
+      <AppState>
       {/* <StackNavigator /> */}
       <MenuLateral />
+      </AppState>
     </NavigationContainer>
+  )
+}
+
+const AppState = ({children}: any) => {
+  return(
+    <AuthProvider>
+      {children}
+    </AuthProvider>
+
   )
 }
 
---
+++ 04-navegacionApp/src/context/AuthContext.tsx
@@ -0,0 +1,42 @@
+
+/* Definir cómo luce o qué información tendré aquí */
+
+import React, { createContext } from "react"
+
+export interface AuthState {
+    isLoggedIn: boolean;
+    username?: string;
+    favoriteIcon?: string;
+}
+
+/* Estado inicial */
+
+export const AuthInitialState: AuthState = {
+    isLoggedIn: false,
+    username: undefined,
+    favoriteIcon: undefined
+}
+
+/* Lo usaremos para decirle a react cómo luce y qué expone el context */
+
+export interface AuthContextProps {
+    authState: AuthState;
+    signIn: () => void;
+}
+
+/* Crear el contexto */
+
+export const AuthContext = createContext({} as AuthContextProps);
+
+/* Componente proveedor del estado */
+
+export const AuthProvider = ({children}: any) => {
+    return(
+        <AuthContext.Provider value={{
+            authState: AuthInitialState,
+            signIn: () => {}
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
