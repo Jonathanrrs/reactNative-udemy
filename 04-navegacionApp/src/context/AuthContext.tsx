394f4b93c08b788d6c8a6615947bc01b9f18aa8a
jonathan <yoni_12rrs@hotmail.com>
2021-08-16 18:59:24 -0500

    Consumir el context - AuthState

--- 04-navegacionApp/src/context/AuthContext.tsx
+++ 04-navegacionApp/src/context/AuthContext.tsx
@@ -31,6 +31,9 @@ export const AuthContext = createContext({} as AuthContextProps);
 /* Componente proveedor del estado */
 
 export const AuthProvider = ({children}: any) => {
+
+    
+
     return(
         <AuthContext.Provider value={{
             authState: AuthInitialState,
--- 04-navegacionApp/src/screens/SettingsScreen.tsx
+++ 04-navegacionApp/src/screens/SettingsScreen.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useContext } from 'react';
 import { Text, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { AuthContext } from '../context/AuthContext';
 import { styles } from '../theme/appTheme';
 
 export const SettingsScreen = () => {
 
     const insets = useSafeAreaInsets();
+
+    const {authState} = useContext(AuthContext);
 
     return (
         <View style={{
@@ -13,6 +17,7 @@ export const SettingsScreen = () => {
             marginTop: insets.top+20
             }}>
             <Text style={styles.title}>SettingsScreen</Text>
+            <Text>{JSON.stringify(authState, null, 4)}</Text>
         </View>
     )
 }
