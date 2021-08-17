e822f85eacb9fabfb9f3f98cd24879709246eaa9
jonathan <yoni_12rrs@hotmail.com>
2021-08-17 16:55:46 -0500

    Disparar el SignIn desde otra pantalla

--- 04-navegacionApp/src/screens/ContactsScreen.tsx
+++ 04-navegacionApp/src/screens/ContactsScreen.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { useContext } from 'react';
+import { Button } from 'react-native';
 import { View, Text } from 'react-native';
+import { AuthContext } from '../context/AuthContext';
+import { styles } from '../theme/appTheme';
 
 export const ContactsScreen = () => {
+
+
+    const { signIn, authState } = useContext(AuthContext)
+
+
     return (
-        <View>
-            <Text>ContactsScreen</Text>
+        <View style={styles.globalMargin}>
+            <Text style={styles.title}>ContactsScreen</Text>
+            {
+                authState.isLoggedIn == false && (
+                    <Button
+                        title="SignIn"
+                        onPress={signIn}
+                    />
+                )
+            }
+
         </View>
     )
 }
