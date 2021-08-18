8e7047bb5e502487eff37cc9f85839465bb69210
jonathan <yoni_12rrs@hotmail.com>
2021-08-18 15:15:40 -0500

    Carousel de tarjetas

--- 05-peliculasApp/src/screens/HomeScreen.tsx
+++ 05-peliculasApp/src/screens/HomeScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { ActivityIndicator, Text, View } from 'react-native';
+import { ActivityIndicator, Dimensions, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { MoviePoster } from '../components/MoviePoster';
 import { useMovies } from '../hooks/useMovies';
 
+import Carousel from 'react-native-snap-carousel';
 
 
+const {width: windowWidth} = Dimensions.get('window');
 
 export const HomeScreen = () => {
 
@@ -22,9 +24,17 @@ export const HomeScreen = () => {
 
     return (
         <View style={{marginTop: top+20}}>
-            <MoviePoster 
+            {/* <MoviePoster 
                 movie={peliculasEnCine[3]}
+            /> */}
+            <View style={{height: 440}}>
+            <Carousel 
+                data={peliculasEnCine}
+                renderItem={({item}: any) => <MoviePoster movie={item}/> }
+                sliderWidth={windowWidth}
+                itemWidth={300}
             />
+            </View>
         </View> 
     )
 }
