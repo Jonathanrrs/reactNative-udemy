5d9480343a7c3d54c9c99c7d4b65dd4a0269a5d0
jonathan <yoni_12rrs@hotmail.com>
2021-08-18 19:47:05 -0500

    Sombra e íconos de nuestra aplicación

--- 05-peliculasApp/src/screens/DetailScreen.tsx
+++ 05-peliculasApp/src/screens/DetailScreen.tsx
@@ -4,6 +4,7 @@ import { Dimensions, Image, StyleSheet, Text, View } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler';
 import { RootStackParams } from '../navigation/Navigation';
 
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const screenHeight = Dimensions.get('screen').height;
 
@@ -14,18 +15,28 @@ export const DetailScreen = ({route}: Props) => {
     const movie = route.params;
     const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
     
+    
 
     return (
         <ScrollView>
         <View style={styles.imageContainer}>
+            <View style={styles.imageBorder}>
             <Image 
                 source={{uri}}
                 style={styles.posterImage}
             />
+            </View>
         </View>
         <View style={styles.marginContainer}>
             <Text style={styles.subTile}>{movie.original_title}</Text>
             <Text style={styles.title}>{movie.title}</Text>
+        </View>
+        <View style={styles.marginContainer}>
+            <Icon
+                name="star-outline"
+                color="grey"
+                size={20}
+            />
         </View>
         </ScrollView>
 
@@ -35,7 +46,7 @@ export const DetailScreen = ({route}: Props) => {
 const styles = StyleSheet.create({
     imageContainer: {
         width: '100%',
-        overflow: 'hidden',
+        
         height: screenHeight * 0.7,
         shadowColor: "#000",
         shadowOffset: {
@@ -46,10 +57,16 @@ const styles = StyleSheet.create({
         shadowRadius: 3.84,
 
         elevation: 9,
-
         borderBottomEndRadius: 25,
         borderBottomStartRadius: 25,
+       
         
+    },
+    imageBorder: {
+        flex: 1,
+        overflow: 'hidden',
+        borderBottomEndRadius: 25,
+        borderBottomStartRadius: 25,
     },
     posterImage: {
         flex: 1,
