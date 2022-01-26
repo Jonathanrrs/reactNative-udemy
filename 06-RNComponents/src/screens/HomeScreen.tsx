9687224bec2825e983b55b7fcc0ddef15acd8f04
jonathan <yoni_12rrs@hotmail.com>
2022-01-25 20:22:26 -0600

    FlatList Separator y Header

--- 06-RNComponents/src/screens/HomeScreen.tsx
+++ 06-RNComponents/src/screens/HomeScreen.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons';
+import { styles } from '../theme/appTheme';
 
 interface MenuItem {
     name: string;
@@ -14,11 +16,18 @@ const menuItems = [
         name: 'Animation 101',
         icon: 'cube-outline',
         components: 'Animation101Screen'
-    }
+    },
+    {
+        name: 'Animation 102',
+        icon: 'albums-outline',
+        components: 'Animation102Screen'
+    },
 ]
 
 
 export const HomeScreen = () => {
+
+    const {top} = useSafeAreaInsets();
 
     const renderMenuItem = (menuItem: MenuItem) => {
         return (
@@ -28,14 +37,36 @@ export const HomeScreen = () => {
         )
     }
 
+    const renderListHeader = () => {
+        return (
+            <View style={{marginTop: top + 20, marginBottom: 20}}>
+                <Text style={styles.title}>Opciones de menú</Text>
+            </View>
+        )
+    }
+
+    const itemSeparator = () => {
+        return (
+            <View
+                style={{
+                    borderBottomWidth: 1,
+                    opacity: 0.4,
+                    marginVertical: 8
+                }}
+            />
+        )
+    }
+
     return (
-        <View style={{flex: 1}}>
-            <FlatList 
+        <View style={{ flex: 1, ...styles.globalMargin }}>
+
+            <FlatList
                 data={menuItems}
-                renderItem={({item}) => renderMenuItem(item)}
+                renderItem={({ item }) => renderMenuItem(item)}
                 keyExtractor={(item) => item.name}
+                ListHeaderComponent={renderListHeader}
+                ItemSeparatorComponent={itemSeparator}
             />
         </View>
     )
 }
-    
