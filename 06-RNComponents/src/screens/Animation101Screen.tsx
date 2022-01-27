c74a0d1f281fe3bfbd72ba098b8e302d016892f7
jonathan <yoni_12rrs@hotmail.com>
2022-01-26 19:23:35 -0600

    useAnimation

--- 06-RNComponents/src/screens/Animation101Screen.tsx
+++ 06-RNComponents/src/screens/Animation101Screen.tsx
@@ -1,44 +1,11 @@
 import React, { useRef } from 'react';
-import { Animated, Button, Easing, StyleSheet, View } from 'react-native';
+import { Animated, Button, StyleSheet, View } from 'react-native';
+import { useAnimation } from '../hooks/useAnimation';
 
 export const Animation101Screen = () => {
 
-
-    const opacity = useRef(new Animated.Value(0)).current;
-    const top = useRef(new Animated.Value(-100)).current;
-
-    const fadeIn = () => {
-        Animated.timing(
-            opacity,
-            {
-                toValue: 1,
-                duration: 300,
-                useNativeDriver: true
-            }
-        ).start(() => console.log('end animation'));
-
-        Animated.timing(
-            top,
-            {
-                toValue: 0,
-                duration: 800,
-                useNativeDriver: true,
-                easing: Easing.bounce
-            }
-        ).start()
-    }
-
-    const fadeOut = () => {
-        Animated.timing(
-            opacity,
-            {
-                toValue: 0,
-                duration: 300,
-                useNativeDriver: true
-            }
-        ).start();
-    }
-
+    const {opacity, position, fadeIn, fadeOut, startMovingPosition} = useAnimation();
+    
     return (
         <View style={styles.container}>
             <Animated.View style={{
@@ -47,13 +14,16 @@ export const Animation101Screen = () => {
                 opacity,
                 /* No se puede la propiedad top */
                 transform: [{
-                    translateY: top
+                    translateX: position
                 }]
             }} />
 
             <Button 
                 title="FadeIn"
-                onPress={fadeIn}
+                onPress={() => {
+                    fadeIn();
+                    startMovingPosition(100)
+                }}
             />
 
             <Button 
