4429c0f75ee56d50d4a13590610cf11190ecc984
jonathan <yoni_12rrs@hotmail.com>
2022-01-26 19:51:26 -0600

    Animated ValueXY

--- 06-RNComponents/src/screens/Animation102Screen.tsx
+++ 06-RNComponents/src/screens/Animation102Screen.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, {useRef} from 'react';
+import { Animated, PanResponder, StyleSheet, View } from 'react-native';
 
 export const Animation102Screen = () => {
+
+    const pan = useRef(new Animated.ValueXY()).current;
+   
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: Animated.event([
+            null,
+            {
+                dx: pan.x,
+                dy: pan.y
+            },
+        ], {
+            useNativeDriver: false
+        }),
+        onPanResponderRelease: () => {
+            Animated.spring(
+                pan,
+                {toValue: {x:0, y:0}, useNativeDriver: false},
+            ).start();
+        }
+    });
+    
+
   return (
       <View style={styles.container}>
-          <View style={styles.purpleBox} />
+          <Animated.View 
+            {...panResponder.panHandlers}
+            style={[pan.getLayout(), styles.purpleBox]}
+          />
       </View>
   )
 };
@@ -16,8 +42,9 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     purpleBox: {
-        backgroundColor: 'red',
+        backgroundColor: '#75CEDB',
         width: 150,
-        height: 150
+        height: 150,
+        borderRadius: 4
     }
 });
