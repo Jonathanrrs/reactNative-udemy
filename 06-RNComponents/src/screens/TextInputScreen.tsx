9e745c451132f535731c3f755270927fab2e8d28
jonathan <yoni_12rrs@hotmail.com>
2022-02-08 15:45:38 -0600

    Componente - TextInput

---
+++ 06-RNComponents/src/screens/TextInputScreen.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+import { HeaderTitle } from '../components/HeaderTitle';
+import { styles } from '../theme/appTheme';
+
+export const TextInputScreen = () => {
+  return (
+    <View style={styles.globalMargin}>
+      <HeaderTitle title="TextInputs" />
+      <TextInput
+        style={stylesScreen.inputStyle}
+      />
+    </View>
+  )
+};
+
+const stylesScreen = StyleSheet.create({
+    inputStyle: {
+      borderWidth: 2,
+      height: 50,
+      paddingHorizontal: 10,
+      borderRadius: 10,
+      borderColor: 'rgba(0,0,0,0.3)'
+    }
+});
