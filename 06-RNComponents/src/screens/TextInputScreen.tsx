b97ae0d9c0733149987f9952124fc99b5648b9f1
jonathan <yoni_12rrs@hotmail.com>
2022-02-08 16:08:44 -0600

    Componente - keyboardavoidingview

--- 06-RNComponents/src/screens/TextInputScreen.tsx
+++ 06-RNComponents/src/screens/TextInputScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { StyleSheet, TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, View, KeyboardAvoidingView, Platform } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import { HeaderTitle } from '../components/HeaderTitle';
 import { styles } from '../theme/appTheme';
 
@@ -21,47 +22,54 @@ export const TextInputScreen = () => {
   }
 
   return (
-    <View style={styles.globalMargin}>
-      <HeaderTitle title="TextInputs" />
-      <TextInput
-        style={stylesScreen.inputStyle}
-        placeholder="Ingrese su nombre"
-        autoCorrect={false}
-        autoCapitalize="words"
-        /* aqui debemos indicarle el campo porque no hay etiqueta name en rn */
-        onChangeText={(value) => onChange(value, 'name')}
-      />
-      <TextInput
-        style={stylesScreen.inputStyle}
-        placeholder="Ingrese su email"
-        autoCorrect={false}
-        autoCapitalize="none"
-        /* aqui debemos indicarle el campo porque no hay etiqueta name en rn */
-        onChangeText={(value) => onChange(value, 'email')}
-        keyboardType="email-address"
-      />
-      <TextInput
-        style={stylesScreen.inputStyle}
-        placeholder="Ingrese su telefono"
-        /* aqui debemos indicarle el campo porque no hay etiqueta name en rn */
-        onChangeText={(value) => onChange(value, 'phone')}
-        keyboardType='numeric'
-      />
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView>
+        <View style={styles.globalMargin}>
+          <HeaderTitle title="TextInputs" />
+          <TextInput
+            style={stylesScreen.inputStyle}
+            placeholder="Ingrese su nombre"
+            autoCorrect={false}
+            autoCapitalize="words"
+            /* aqui debemos indicarle el campo porque no hay etiqueta name en rn */
+            onChangeText={(value) => onChange(value, 'name')}
+          />
+          <TextInput
+            style={stylesScreen.inputStyle}
+            placeholder="Ingrese su email"
+            autoCorrect={false}
+            autoCapitalize="none"
+            /* aqui debemos indicarle el campo porque no hay etiqueta name en rn */
+            onChangeText={(value) => onChange(value, 'email')}
+            keyboardType="email-address"
+          />
+          <HeaderTitle title={JSON.stringify(form, null, 3)} />
+          <HeaderTitle title={JSON.stringify(form, null, 3)} />
 
-    <HeaderTitle title={JSON.stringify(form, null, 3)} />
-
-    </View>
+          <TextInput
+            style={stylesScreen.inputStyle}
+            placeholder="Ingrese su telefono"
+            /* aqui debemos indicarle el campo porque no hay etiqueta name en rn */
+            onChangeText={(value) => onChange(value, 'phone')}
+            keyboardType='numeric'
+          />
+        </View>
+        <View style={{height:100}}/>
+      </ScrollView>
+    </KeyboardAvoidingView>
   )
 };
 
 const stylesScreen = StyleSheet.create({
-    inputStyle: {
-      borderWidth: 2,
-      height: 50,
-      paddingHorizontal: 10,
-      borderRadius: 10,
-      borderColor: 'rgba(0,0,0,0.3)',
-      marginVertical: 10,
-      color: 'black'
-    }
+  inputStyle: {
+    borderWidth: 2,
+    height: 50,
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    borderColor: 'rgba(0,0,0,0.3)',
+    marginVertical: 10,
+    color: 'black'
+  }
 });
