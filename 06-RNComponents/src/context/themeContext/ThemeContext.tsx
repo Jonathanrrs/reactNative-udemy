b54f0ee48aa0eafad7068fa7190b4db969935573
jonathan <yoni_12rrs@hotmail.com>
2022-03-02 16:37:08 -0600

    Cambio de tema basado en el Sistema Operativo

--- 06-RNComponents/src/context/themeContext/ThemeContext.tsx
+++ 06-RNComponents/src/context/themeContext/ThemeContext.tsx
@@ -1,5 +1,6 @@
-import React, {createContext, useReducer} from 'react';
-import {lightTheme, themeReducer, ThemeState} from './themeReducer';
+import React, {createContext, useEffect, useReducer} from 'react';
+import {Appearance, AppState, useColorScheme} from 'react-native';
+import {lightTheme, themeReducer, ThemeState, darkTheme} from './themeReducer';
 
 interface ThemeContextProps {
   theme: ThemeState;
@@ -10,7 +11,29 @@ interface ThemeContextProps {
 export const ThemeContext = createContext({} as ThemeContextProps);
 
 export const ThemeProvider = ({children}: any) => {
-  const [theme, dispatch] = useReducer(themeReducer, lightTheme);
+  /* para ios */
+  // const colorScheme = useColorScheme();
+  const [theme, dispatch] = useReducer(
+    themeReducer,
+    Appearance.getColorScheme() === 'dark' ? darkTheme : lightTheme,
+  );
+
+  /* SOLO FUNCIONA EN IOS */
+  // useEffect(() => {
+  //   colorScheme === 'light' ? setLightTheme() : setDarkTheme();
+  // }, [colorScheme]);
+
+  /* obtener el color del sistema */
+  useEffect(() => {
+    AppState.addEventListener('change', status => {
+      if (status === 'active') {
+        Appearance.getColorScheme() === 'light'
+          ? setLightTheme()
+          : setDarkTheme();
+      }
+    });
+  }, []);
+
   const setDarkTheme = () => {
     dispatch({type: 'set_dark_theme'});
     console.log('setdark');
