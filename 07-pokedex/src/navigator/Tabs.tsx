06e99a087d973a5c412e7d25b60ad78ece0cbaaf
jonathan <yoni_12rrs@hotmail.com>
2022-03-04 18:43:58 -0600

    Implementar Tabs en nuestra aplicación

---
+++ 07-pokedex/src/navigator/Tabs.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {Platform, StyleSheet} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Navigator} from './Navigator';
+import {SearchScreen} from '../screens/SearchScreen';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
+
+export const Tabs = () => {
+  return (
+    <Tab.Navigator
+      sceneContainerStyle={styles.container}
+      screenOptions={{
+        tabBarActiveTintColor: '#5856D5',
+        tabBarStyle: {
+          borderWidth: 0,
+          elevation: 0,
+          position: 'absolute',
+          backgroundColor: 'rgba(255,255,255, 0.92)',
+          height: Platform.OS === 'ios' ? 80 : 60,
+        },
+        headerShown: false,
+      }}>
+      <Tab.Screen
+        name="MainScreen"
+        component={Navigator}
+        options={{
+          tabBarLabel: 'Listado',
+          tabBarIcon: ({color}) => (
+            <Icon name="list-outline" color={color} size={25} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="SearchScreen"
+        component={SearchScreen}
+        options={{
+          tabBarLabel: 'Buscar',
+          tabBarIcon: ({color}) => (
+            <Icon name="search-outline" color={color} size={25} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'red',
+  },
+});
