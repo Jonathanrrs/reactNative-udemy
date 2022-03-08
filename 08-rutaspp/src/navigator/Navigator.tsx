7af8aa0d1c8f3900e78314409a6885a504ce0392
jonathan <yoni_12rrs@hotmail.com>
2022-03-08 14:52:33 -0600

    Solicitar y revisar permisos - Check y Request Permission

--- 08-rutaspp/src/navigator/Navigator.tsx
+++ 08-rutaspp/src/navigator/Navigator.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {MapScreen} from '../screens/MapScreen';
 import {PermissionsScreen} from '../screens/PermissionsScreen';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export const Navigator = () => {
   return (
     <Stack.Navigator
+      // initialRouteName="PermissionsScreen"
       screenOptions={{
         headerShown: false,
-        cardStyle: {backgroundColor: 'white'},
+        contentStyle: {backgroundColor: 'white'},
       }}>
-      <Stack.Screen name="MapScreen" component={MapScreen} />
       <Stack.Screen name="PermissionsScreen" component={PermissionsScreen} />
+      <Stack.Screen name="MapScreen" component={MapScreen} />
     </Stack.Navigator>
   );
 };
