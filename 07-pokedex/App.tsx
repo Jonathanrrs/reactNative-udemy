1fc7cfed1c24f80b34ca87c09eba0efd8814db34
jonathan <yoni_12rrs@hotmail.com>
2022-03-07 20:15:00 -0600

    logbox quitado

--- 07-pokedex/App.tsx
+++ 07-pokedex/App.tsx
@@ -3,6 +3,11 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 // import {Navigator} from './src/navigator/Navigator';
 import {Tabs} from './src/navigator/Tabs';
+import {LogBox} from 'react-native';
+
+LogBox.ignoreLogs([
+  "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+]);
 
 const App = () => {
   return (
