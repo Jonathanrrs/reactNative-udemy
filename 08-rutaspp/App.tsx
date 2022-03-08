8c4864a0cc14f19320c3d6317d68348f067cc987
jonathan <yoni_12rrs@hotmail.com>
2022-03-08 13:26:53 -0600

    Inicio de proyecto

---
+++ 08-rutaspp/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const App = () => {
+  return <Icon name="star-outline" color="red" size={50} />;
+};
+
+export default App;
