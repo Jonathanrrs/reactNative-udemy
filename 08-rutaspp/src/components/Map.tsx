e2df96aa4f0e170db4849f16e24ff7b6b424bc52
jonathan <yoni_12rrs@hotmail.com>
2022-03-09 13:11:01 -0600

    CustomHook - useLocation

--- 08-rutaspp/src/components/Map.tsx
+++ 08-rutaspp/src/components/Map.tsx
@@ -1,27 +1,25 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {StyleSheet} from 'react-native';
 import MapView from 'react-native-maps';
-import Geolocation from '@react-native-community/geolocation';
+import {useLocation} from '../hooks/useLocation';
+import {LoadingScreen} from '../screens/LoadingScreen';
 
 export const Map = () => {
-  useEffect(() => {
-    Geolocation.getCurrentPosition(
-      info => console.log(info),
-      err => console.log({err}),
-      {
-        enableHighAccuracy: true,
-      },
-    );
-  }, []);
+  const {hasLocation, initialPosition} = useLocation();
+
+  if (!hasLocation) {
+    return <LoadingScreen />;
+  }
 
   return (
     <>
       <MapView
         style={styles.container}
         showsUserLocation
+        /* ! es porque sabemos que siempre tendrá el dato */
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: initialPosition!.latitude,
+          longitude: initialPosition!.longitud,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}>
