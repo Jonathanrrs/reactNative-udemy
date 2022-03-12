a972e2437e0f7e1fadc15d8b6a3d0c6ed155319c
jonathan <yoni_12rrs@hotmail.com>
2022-03-11 18:44:39 -0600

    Diseño del login

---
+++ 10-productosApp/src/components/Background.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+export const Background = () => {
+  return <View style={styles.container} />;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    backgroundColor: '#5856D6',
+    width: 1000,
+    height: 1200,
+    transform: [{rotate: '-70deg'}],
+    top: -250,
+  },
+});
---
+++ 10-productosApp/src/theme/loginTheme.tsx
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+export const loginStyles = StyleSheet.create({
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 30,
+  },
+  label: {
+    marginTop: 25,
+    fontWeight: 'bold',
+  },
+});
