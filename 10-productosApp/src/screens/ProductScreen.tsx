d1df1d17b757bb2e10de617683c8fc7046f00361
jonathan <yoni_12rrs@hotmail.com>
2022-03-15 13:52:15 -0600

    Diseño de la pantalla de producto

--- 10-productosApp/src/screens/ProductScreen.tsx
+++ 10-productosApp/src/screens/ProductScreen.tsx
@@ -1,6 +1,13 @@
 import React, {useEffect} from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {Text, View, StyleSheet} from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  Button,
+} from 'react-native';
 import {ProductsStackParams} from '../navigator/ProductsNavigator';
 
 interface Props
@@ -15,15 +22,46 @@ export const ProductScreen = ({route, navigation}: Props) => {
   }, []);
 
   return (
-    <View>
-      <Text style={styles.txt}>{id}</Text>
-      <Text style={styles.txt}>{name}</Text>
+    <View style={styles.container}>
+      <ScrollView>
+        <Text style={styles.label}>Nombre del producto</Text>
+        <TextInput placeholder="Producto" style={styles.txtInput} />
+
+        {/* picker / selector */}
+        <Text style={styles.label}>Categoría:</Text>
+        <Button title="Guardar" onPress={() => {}} color="#5856D6" />
+        <View style={styles.containerBtns}>
+          <Button title="Cámara" onPress={() => {}} color="#5856D6" />
+          <Button title="Galeria" onPress={() => {}} color="#5856D6" />
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  txt: {
+  container: {
+    flex: 1,
+    marginTop: 10,
+    marginHorizontal: 20,
+  },
+  label: {
     color: 'black',
+    fontSize: 18,
+  },
+  txtInput: {
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 20,
+    borderColor: 'rgba(0,0,0,0.2)',
+    height: 45,
+    marginTop: 5,
+    marginBottom: 15,
+  },
+  containerBtns: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 10,
   },
 });
