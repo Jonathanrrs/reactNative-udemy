74ff549587c75dd68a5585bf6aaf6c43c81cf648
jonathan <yoni_12rrs@hotmail.com>
2022-03-15 13:39:32 -0600

    Navegar a la pantalla de Producto

--- 10-productosApp/src/screens/ProductScreen.tsx
+++ 10-productosApp/src/screens/ProductScreen.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {Text, View, StyleSheet} from 'react-native';
+import {ProductsStackParams} from '../navigator/ProductsNavigator';
 
-export const ProductScreen = () => {
+interface Props
+  extends NativeStackScreenProps<ProductsStackParams, 'ProductScreen'> {}
+
+export const ProductScreen = ({route, navigation}: Props) => {
+  const {id, name = ''} = route.params;
+
+  useEffect(() => {
+    navigation.setOptions({title: name !== '' ? name : 'Nuevo producto'});
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <View>
-      <Text>ProductScreen</Text>
+      <Text style={styles.txt}>{id}</Text>
+      <Text style={styles.txt}>{name}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  txt: {
+    color: 'black',
+  },
+});
--- 10-productosApp/src/screens/ProductsScreen.tsx
+++ 10-productosApp/src/screens/ProductsScreen.tsx
@@ -1,9 +1,28 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {Text, View, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
 import {ProductsContext} from '../context/ProductsContext';
+import {ProductsStackParams} from '../navigator/ProductsNavigator';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
-export const ProductsScreen = () => {
+interface Props
+  extends NativeStackScreenProps<ProductsStackParams, 'ProductsScreen'> {}
+
+export const ProductsScreen = ({navigation}: Props) => {
   const {products, loadProducts} = useContext(ProductsContext);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          activeOpacity={0.8}
+          onPress={() => navigation.navigate('ProductScreen', {})}>
+          <Text style={styles.btnRight}>Agregar</Text>
+        </TouchableOpacity>
+      ),
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   /* Pull to refresh */
   return (
     <View style={styles.container}>
@@ -11,7 +30,14 @@ export const ProductsScreen = () => {
         data={products}
         keyExtractor={p => p._id}
         renderItem={({item}) => (
-          <TouchableOpacity activeOpacity={0.8}>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            onPress={() =>
+              navigation.navigate('ProductScreen', {
+                id: item._id,
+                name: item.nombre,
+              })
+            }>
             <Text style={styles.productName}>{item.nombre}</Text>
           </TouchableOpacity>
         )}
@@ -34,5 +60,9 @@ const styles = StyleSheet.create({
     borderBottomWidth: 2,
     marginVertical: 5,
     borderBottomColor: 'rgba(0,0,0,0.1)',
+  },
+  btnRight: {
+    marginRight: 10,
+    color: 'black',
   },
 });
